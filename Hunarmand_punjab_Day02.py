7d388fa1535ac4689f38a0c14333c56bcf766913
mahamfayyazml36-dot <mahamfayyazml36@gmail.com>
2026-08-03 12:24:10 +0500

    Hunarmand punjab Day02
    Hunarmand Punjab Day 2 - Variables, casting, data types, case sensitivity and variable naming practice.

---
+++ Hunarmand_punjab_Day02.py
@@ -0,0 +1,43 @@
+                    # Variable
+
+name = "Maham Fayyaz" 
+age = 19
+favourite_color = "Black"
+country = "Pakistan"
+student = True 
+print(name, age, favourite_color, country, student)
+print(name)
+print(age)
+print(favourite_color)
+print(country)
+print(student)  
+
+                          #Casting
+
+X = str(19)
+y = int(19)
+Z = float(20)
+print(X)
+print(y)
+print(Z)
+                          # Type
+
+print(type(X))
+print(type(y))
+print(type(Z))
+                          # Case_Sensitive
+
+name = "Jannat" 
+Name = "Noor Salam"
+print(name)
+print(Name)  
+                          # Variable Naming
+#1. camel-case
+studentName = "Mafia Fayyaz"
+print(StudentName)
+#2. pascal_case
+SudentName = "Jannat Fayyaz"
+print(studentName)
+#3. Snake_case
+student_name = "Maria Fayyaz"
+print(Student_Name)
