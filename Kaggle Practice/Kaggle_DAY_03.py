96fe1016a2ca6ebf0c9feb90b855d062c99c3d02
Maham Fayyaz <mahamfayyazml36@gmail.com>
2026-08-10 14:00:59 +0500

    Kaggle day 3 practice

---
+++ Kaggle Practice/Kaggle_DAY_03.py
@@ -0,0 +1,87 @@
+# Data Types
+
+# integer 
+
+# 1.
+
+x = 12
+y = 3
+print(x + y)
+print(type(x))
+print(type(y))
+
+# Float
+
+# 2.
+
+a = 15.55
+print(a)
+print(type(a))
+
+# 3.
+
+z = 2.5256
+print(z)
+print(type(z))
+
+y = 50/2
+print(y)
+print(type(y))
+
+x = round(y, 5)
+print(x)
+print(type(x))
+ 
+b = 250.
+print(b)
+print(type(b))
+
+# Boolean
+
+# 4. 
+
+student = True
+print(student)
+print(type(student))
+
+# 5.
+
+numbers = 5 > 3
+print(numbers)
+print(type(numbers)) 
+
+# 6.
+has_job = False
+print(has_job)
+print(type(has_job))
+# 7.
+number = 5 < 3
+print(number)
+print(type(number))
+
+# 8.
+number1 = not number
+print(number1)
+print(type(number1))
+
+# String
+
+# 9.
+intro = "Hello World!"
+print(intro)
+print(len(intro))
+print(type(intro))
+# 10.
+quick = "1256.36"
+print(quick)
+print(float(quick))
+print(type(quick))
+# 11.
+list_check = "ABC"*3
+print(list_check)
+# 12.
+
+teen_age = "hhh" + "yyy"
+print(teen_age)
+print(type(teen_age))
+
